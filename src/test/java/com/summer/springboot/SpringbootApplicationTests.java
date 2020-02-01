dd86d5a2d77dd31781390b7986d806d0ef070fb9
summer <xiahaoyang_x@163.com>
2020-02-01 16:47:11 +0800

    .propertires文件注入问题
    spring和springboot运用注解注入

--- src/test/java/com/summer/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/summer/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,17 @@
 package com.summer.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Autowired
+    Book book;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println(book);
     }
 
 }
